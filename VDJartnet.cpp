96f5f6bad0a98ab32bbaeeff60ed17d04ca1fafd
binaryDNA--while1-program <jonathantanner@Jonathans-Mac-mini.local>
2017-04-20 23:55:58 +0100

    Added provision for linking to VirtualDJ
    Added provision for linking to VirtualDJ

--- VDJartnet.cpp
+++ VDJartnet.cpp
@@ -19,6 +19,14 @@
 //
 //You should have received a copy of the GNU General Public License
 //along with VDJartnet.  If not, see <http://www.gnu.org/licenses/>.
+//
+//Additional permission under GNU GPL version 3 section 7
+//
+//If you modify this Program, or any covered work, by linking or
+//combining it with VirtualDJ, the licensors of this Program grant you
+//additional permission to convey the resulting work.
+//{Corresponding Source for a non-source form of such a combination shall not
+//include the source code for the parts of VirtualDJ used as well as that of the covered work.}
 
 #define ZED_NET_IMPLEMENTATION
 #define GLOBALIMPLEMENTATION
