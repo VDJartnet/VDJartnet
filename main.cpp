c21a74b3a6b787a2f2ddcabae97c843f60ca3607
binaryDNA--while1-program <jonathantanner@Jonathans-Mac-mini.local>
2017-04-20 22:29:58 +0100

    Added License Notices

--- main.cpp
+++ main.cpp
@@ -5,6 +5,20 @@
 //  Created by Jonathan Tanner on 09/02/2017.
 //  Copyright © 2017 Jonathan Tanner. All rights reserved.
 //
+//This file is part of VDJartnet.
+//
+//VDJartnet is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//VDJartnet is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with VDJartnet.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "VDJartnet.hpp"
 
