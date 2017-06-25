653af2ce51c16d59582ceacca8f32088f34d53b1
binaryDNA--while1-program <jonathantanner@Jonathans-Mac-mini.local>
2017-06-25 18:34:35 +0100

    Test if compiler failure is shown in AppVeyor

--- buildWin.bat
+++ buildWin.bat
@@ -1,3 +1,3 @@
 "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall" x86
 cl /Fo.\ConfigWin.obj /clr /MD .\ConfigWin.cpp
-cl /LD /EHsc /MD .\VDJartnet.cpp .\main.cpp .\ConfigWin.obj
+cl /Fe.\VDJartnet.dll /LD /EHsc /MD .\VDJartnet.cpp .\main.cpp .\ConfigWin2.obj
