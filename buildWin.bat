b384fee741cf6bdcaaafbe76817db7e31e9b086c
binaryDNA--while1-program <jonathantanner@Jonathans-Mac-mini.local>
2017-06-25 17:51:40 +0100

    Adjust buildWin.bat

--- buildWin.bat
+++ buildWin.bat
@@ -1,3 +1,3 @@
-\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall x86
+C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall x86
 cl /FoC:\ConfigWin.obj /clr /MD .\ConfigWin.cpp
 cl /FeC:\VDJartnet.dll /LD /EHsc /MD .\VDJartnet.cpp .\main.cpp C:\ConfigWin.obj
