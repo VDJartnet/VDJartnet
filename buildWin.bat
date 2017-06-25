d5fd3c80efa7553058fdbb39b559f5675c7f9441
binaryDNA--while1-program <jonathantanner@Jonathans-Mac-mini.local>
2017-06-25 18:08:01 +0100

    Make build files local

--- buildWin.bat
+++ buildWin.bat
@@ -1,3 +1,3 @@
 "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall" x86
-cl /FoC:\ConfigWin.obj /clr /MD .\ConfigWin.cpp
-cl /Fe.\VDJartnet.dll /LD /EHsc /MD .\VDJartnet.cpp .\main.cpp C:\ConfigWin.obj
+cl /Fo.\ConfigWin.obj /clr /MD .\ConfigWin.cpp
+cl /Fe.\VDJartnet.dll /LD /EHsc /MD .\VDJartnet.cpp .\main.cpp .\ConfigWin.obj
