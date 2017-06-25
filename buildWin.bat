4af7d8c5f7b212236da3c0ac3c77e9a91cca9a03
binaryDNA--while1-program <jonathantanner@Jonathans-Mac-mini.local>
2017-06-25 18:53:44 +0100

    Remove echo

--- buildWin.bat
+++ buildWin.bat
@@ -1,7 +1,3 @@
-ECHO ON
 "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall" x86
-ECHO ON
 cl /Fo.\ConfigWin.obj /clr /MD .\ConfigWin.cpp
-ECHO ON
 cl /Fe.\VDJartnet.dll /LD /EHsc /MD .\VDJartnet.cpp .\main.cpp .\ConfigWin.obj
-ECHO ON
