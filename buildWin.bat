e22b283fbb03ab55ef6efa492e0dabddd7b2b122
binaryDNA--while1-program <jonathantanner@Jonathans-Mac-mini.local>
2017-06-25 18:48:55 +0100

    Defensively "ECHO ON"

--- buildWin.bat
+++ buildWin.bat
@@ -1,4 +1,7 @@
 ECHO ON
 "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall" x86
+ECHO ON
 cl /Fo.\ConfigWin.obj /clr /MD .\ConfigWin.cpp
+ECHO ON
 cl /Fe.\VDJartnet.dll /LD /EHsc /MD .\VDJartnet.cpp .\main.cpp .\ConfigWin.obj
+ECHO ON
