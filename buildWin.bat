d3f8c5646bd06af6b2aa4720b64a650e7393f86d
binaryDNA--while1-program <jonathantanner@Jonathans-Mac-mini.local>
2017-06-25 18:46:18 +0100

    Try turning on echoing

--- buildWin.bat
+++ buildWin.bat
@@ -1,3 +1,4 @@
+ECHO ON
 "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall" x86
 cl /Fo.\ConfigWin.obj /clr /MD .\ConfigWin.cpp
 cl /Fe.\VDJartnet.dll /LD /EHsc /MD .\VDJartnet.cpp .\main.cpp .\ConfigWin.obj
