470fbb60b1c965e7a84cca9394f5e653444b220f
sh6397 <sam.hart6397@gmail.com>
2017-09-04 22:08:21 +0100

    Update Wix

--- buildPackageWin.bat
+++ buildPackageWin.bat
@@ -1,5 +1,12 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall" x86
+set "VSCMD_START_DIR=%CD%"
+
+::call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall" x86
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
 
 cl /Fo.\ConfigWin.obj /c /clr /MD .\ConfigWin.cpp
 
 cl /Fe.\VDJartnet.dll /LD /EHsc /MD .\VDJartnet.cpp .\main.cpp .\ConfigWin.obj /link /LIBPATH:"C:\Program Files\Microsoft Visual Studio 14.0\VC\lib"
+
+candle winInstaller.wxs
+
+light -ext WixUIExtension winInstaller.wixobj
