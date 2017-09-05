7fb201f07d80baf2e139c1502ee182c2ff2075d0
sh6397 <sam.hart6397@gmail.com>
2017-09-05 23:18:50 +0100

    Update Wix

--- buildPackageWin.bat
+++ buildPackageWin.bat
@@ -4,9 +4,17 @@ set "VSCMD_START_DIR=%CD%"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
 
 cl /Fo.\ConfigWin.obj /c /clr /MD .\ConfigWin.cpp
-
 cl /Fe.\VDJartnet.dll /LD /EHsc /MD .\VDJartnet.cpp .\main.cpp .\ConfigWin.obj /link /LIBPATH:"C:\Program Files\Microsoft Visual Studio 14.0\VC\lib"
 
-candle winInstaller.wxs
+echo README > README.rtf
+mkdir "%TEMP%\pandoc\"
+powershell -command "& { (New-Object Net.WebClient).DownloadFile(\"https://github.com/jgm/pandoc/releases/download/1.19.2.1/pandoc-1.19.2.1-windows.msi\", \"%TEMP%\pandoc.msi\") }"
+start /wait msiexec /i "%TEMP%\pandoc.msi" /qn TARGETDIR="%TEMP%\pandoc" /Lvx* "%TEMP%\pandoc_install.log"
 
-light -ext WixUIExtension winInstaller.wixobj
+"%TEMP%\pandoc\pandoc.exe" -s -S -f markdown_github -t rtf .\README.md -o .\README.rtf
+::"%APPDATA%\..\Local\Pandoc\pandoc.exe" -s -S -f markdown_github -t rtf .\README.md -o .\README.rtf
+
+candle winInstaller.wxs ReadmeDlg.wxs
+light -ext WixUIExtension -out VDJartnet.msi winInstaller.wixobj ReadmeDlg.wixobj
+
+::rmdir /s /q "%TEMP%\pandoc\"
